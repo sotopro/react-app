ef30f1b5b2d3aeaf19bf9ee74591e9dddcca6aa5
sotopro <dasg.1993@gmail.com>
2022-11-24 08:57:34 -0500

    [feature] - create button component and style

---
+++ src/components/button/index.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import './styles.css';
+
+const Button = () => {
+    return (
+        <button className="button-primary">Click me</button>
+    )
+}
+
+export default Button;
