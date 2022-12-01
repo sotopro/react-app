7487dfabf37738587426c83348a751d5407d995e
sotopro <dasg.1993@gmail.com>
2022-12-01 09:58:19 -0500

    [feature]- create header componet refactor styles & open siderbar

--- src/App.js
+++ src/App.js
@@ -1,33 +1,30 @@
-import React, { useState} from 'react';
+import React, { useState, useEffect} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Button from './components/button';
 import Sidebar from './components/sidebar';
+import Header from './components/header';
 import { PRODUCTS } from './constants/data/products';
 import Card from './components/card';
 
 function App() {
   const [isOpen, setIsOpen] = useState(false);
   const [showProducts, setShowProducts] = useState(false);
-  const onHandlerClick = () => {
-    setIsOpen(!isOpen);
-    setShowProducts(!showProducts);
-  }
-  return (
-    <div className="App">
-      <Sidebar onClose={onHandlerClick} isOpen={isOpen} />
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Button text='Click me' onHandlerClick={onHandlerClick}/>
-        {showProducts ? (
-          <div className='products-container'>
-          {PRODUCTS.map((product) => (
-            <Card product={product} key={product.name} />
-          ))}
-        </div>
-        ) : null }
 
-      </header>
+  const onHandlerCart = () => {
+    setIsOpen(!isOpen);
+  }
+
+  return (
+    <div className="container">
+      <Sidebar onClose={onHandlerCart} isOpen={isOpen} />
+      <Header numbersOfItems={0} onHandlerCart={onHandlerCart} />
+      <h1>Productos destacados</h1>
+        <div className='products-container'>
+        {PRODUCTS.map((product) => (
+          <Card product={product} key={product.name} />
+        ))}
+      </div>
     </div>
   );
 }
